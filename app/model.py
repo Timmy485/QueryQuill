3cc3c419282dd724297c84076f688f616a970357
Timmy485 <yeboahtimothy@gmail.com>
2023-09-27 14:17:50 +0000

    feat: Generate Passage Embeddings with SentenceTransformers Model
    This commit adds code to generate high-quality embeddings for the extracted passages using a SentenceTransformers model. The chosen model provides state-of-the-art text embeddings.
    
    Changes made:
    - Used SentenceTransformers for embeddings
    - Saved embeddings along with passages and metadata in passage_metadata_emb.csv
    - CSV columns: Passage, Metadata, Embedding

---
+++ app/model.py
@@ -0,0 +1,24 @@
+import csv
+import json
+from sentence_transformers import SentenceTransformer
+
+def generate_embeddings_and_save(csv_input_path, csv_output_path):
+    # Load the model
+    model = SentenceTransformer('paraphrase-distilroberta-base-v1')
+    
+    with open(csv_input_path, "r", encoding="utf-8") as csvfile, open(csv_output_path, "w", newline='', encoding="utf-8") as outfile:
+        reader = csv.DictReader(csvfile)
+        fieldnames = ["Passage", "Metadata", "Embedding"]
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for row in reader:
+            passage = row["Passage"]
+            metadata = row["Metadata"]
+            embedding = model.encode(passage, convert_to_tensor=True).tolist()
+            
+            writer.writerow({
+                "Passage": passage,
+                "Metadata": metadata,
+                "Embedding": embedding
+            })
